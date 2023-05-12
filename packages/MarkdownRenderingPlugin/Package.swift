1d6c0db8243f46924b5a8dd89f68db21a6b660e9
sirily11 <sirily1997@gmail.com>
2023-05-12 16:13:16 +0800

    fix: update package.swift

--- packages/MarkdownRenderingPlugin/Package.swift
+++ packages/MarkdownRenderingPlugin/Package.swift
@@ -18,6 +18,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(path: "../MenuBarSDK"),
+        .package(url: "https://github.com/gonzalezreal/swift-markdown-ui", exact: "2.1.0"),
+        .package(url: "https://github.com/JohnSundell/Splash", exact: "0.16.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,6 +28,8 @@ let package = Package(
             name: "MarkdownRenderingPlugin",
             dependencies: [
                 .product(name: "MenuBarSDK", package: "MenuBarSDK"),
+                .product(name: "MarkdownUI", package: "swift-markdown-ui"),
+                .product(name: "Splash", package: "Splash"),
             ]),
         .testTarget(
             name: "MarkdownRenderingPluginTests",
